fa1e104773aad8bc2364cf7af3cc1f5a3c7c8c55
RiverYale <1181065460@qq.com>
2023-03-15 14:50:47 +0800

    功能加载方式优化，缓解阻塞

--- Bangumi快捷播放/BangumiEasyPlay.js
+++ Bangumi快捷播放/BangumiEasyPlay.js
@@ -3,7 +3,7 @@
 // @description  首页追番卡片显示中文标题，浮动卡片增加资源搜索，可添加对应集数播放源，浮动卡片状态实时改变无需刷新
 // @namespace    https://github.com/RiverYale/Userscripts/
 // @homepage     https://riveryale.github.io/Userscripts/
-// @version      2.9
+// @version      3.0
 // @author       RiverYale
 // @match        *://bangumi.tv
 // @match        *://bgm.tv
@@ -25,35 +25,45 @@ var src_dict = {					// 网址格式，番剧ID: [资源ID, 播放线路, 总体
 		search: "https://www.agemys.net/search?query=${keyword}&page=1",
 		329114: [20220076, 2, 0],		// 想要成为影之实力者
 		339326: [20220134, 2, 0],		// 异世界舅舅
+		330054: [20210126, 2, 0],		// 魔王学院的不适合者 第二季
+		402223: [20230025, 2, -11],		// 期待在地下城邂逅有错吗 Ⅳ 深章 灾厄篇
+		378862: [20220268, 2, 0],		// 别当欧尼酱了！
+		320839: [20220084, 2, -12],		// 虚构推理 第二季
 	},
-    "樱花动漫": {
-		pattern: "http://www.yinghuacd.com/v/${id}-${ep}.html",
-		search: "http://www.yinghuacd.com/search/${keyword}/",
-        329114: [5444, 1, 0],			// 想要成为影之实力者
-		339326: [5531, 1, 0],			// 异世界舅舅
-    },
+	"樱花动漫": {
+		pattern: "https://www.yhdmp.cc/vp/${id}-${ch}-${ep}.html",
+		search: "https://www.yhdmp.cc/s_all?ex=1&kw=${keyword}",
+		329114: [22158, 1, -1],			// 想要成为影之实力者
+		339326: [22216, 1, -1],			// 异世界舅舅
+		330054: [21208, 1, -1],			// 魔王学院的不适合者 第二季
+		402223: [22083, 1, -12],		// 期待在地下城邂逅有错吗 Ⅳ 深章 灾厄篇
+		378862: [22350, 1, -1],			// 别当欧尼酱了！
+		320839: [22166, 1, -13],		// 虚构推理 第二季
+	},
 	"Bimi": {
 		pattern: "https://www.bimiacg4.net/bangumi/${id}/play/${ch}/${ep}/",
 		search: "https://www.bimiacg4.net/vod/search/wd/${keyword}",
-        329114: [8150, 1, 0],			// 想要成为影之实力者
+		329114: [8150, 1, 0],			// 想要成为影之实力者
+		330054: [2783, 1, 0],			// 魔王学院的不适合者 第二季
+		378862: [8227, 1, 0],			// 别当欧尼酱了！
+		320839: [8270, 1, -12],			// 虚构推理 第二季
 	},
-    "新番组": {
+	"新番组": {
 		pattern: "https://bangumi.online",
 		search: "https://bangumi.online",
-        329114: "https://bangumi.online/watch/vb6D7_0qgvtzvkAP-",		// 想要成为影之实力者
-        218708: "https://www.bilibili.com/bangumi/play/ep164970",		// 比宇宙更遥远的地方
-    },
+		329114: "https://bangumi.online/watch/vb6D7_0qgvtzvkAP-",		// 想要成为影之实力者
+		218708: "https://www.bilibili.com/bangumi/play/ep164970",		// 比宇宙更遥远的地方
+	},
 };
 /*================= 更新脚本前注意保存自己修改的内容！ =================*/
 
-if($(".loginPanel").length == 1) {
-	return;
-}
+if($(".loginPanel").length == 1) return;
 
 /* 标题中日文对调 */
 var titles = Array.from($(".tinyHeader .textTip:not(.prgCheckIn)"))		// 平铺模式
 	.concat(Array.from($(".l.textTip")))		// 列表模式右侧
-titles.forEach(title => {
+if(titles.length == 0) return;
+var handleTitle_A = function(title) {
 	var text = $(title).text();
 	var data_original_title = $(title).attr("data-original-title");
 	if(!data_original_title){
@@ -61,12 +71,12 @@ titles.forEach(title => {
 	}
 	$(title).text(data_original_title);
 	$(title).attr("data-original-title", text);
-});
-if(titles.length == 0) {
-	return;
 }
-titles = Array.from($(".subjectItem.title.textTip"))		// 列表模式左侧 - 1, 2, 3
 titles.forEach(title => {
+	setTimeout(handleTitle_A, 500, title);
+});
+titles = Array.from($(".subjectItem.title.textTip"))		// 列表模式左侧 - 1, 2, 3
+var handleTitle_B = function(title) {
 	var text = $(title).find('span').text();
 	var data_original_title = $(title).attr("data-original-title");
 	if(!data_original_title){
@@ -78,6 +88,9 @@ titles.forEach(title => {
 	var preALink = $(title).prev().prev();
 	var preALink_title = $(preALink).attr('data-original-title');
 	$(preALink).attr('data-original-title', preALink_title.replace(text, data_original_title));
+}
+titles.forEach(title => {
+	setTimeout(handleTitle_B, 750, title);
 });
 
 
@@ -96,6 +109,9 @@ $(_input).click(() => {
 	autoMark = $(_input).prop("checked");
 });
 
+
+/* 为已出集数添加资源链接 */
+var epLinkList = Array.from($(".load-epinfo"));
 var getSrcHref = function(dict, subid, ep) {
 	var value = dict[subid];
 	ep = Number(ep);
@@ -115,10 +131,7 @@ var getSrcHref = function(dict, subid, ep) {
 	}
 	return '';
 }
-
-/* 为已出集数添加资源链接 */
-var epLinkList = Array.from($(".load-epinfo"));
-epLinkList.forEach(epLink => {
+var handleEpLinkList = function(epLink) {
 	// 标记该条目下集数的偏移量
 	$(epLink.rel).attr("ep_offset", $(epLink).parent().index());
 
@@ -184,7 +197,11 @@ epLinkList.forEach(epLink => {
 			// $(epLink).css({"color":"#00F", "background-color":"#e0e0e0", "border":"1px solid #b6b6b6"});
 		});
 	}
+}
+epLinkList.forEach(epLink => {
+	setTimeout(handleEpLinkList, 1000, epLink);
 });
+
 
 // 根据状态更新单集进度情况面板
 var updataEpStatusTool = function(prg, type) {
@@ -246,9 +263,10 @@ var updataEpStatusTool = function(prg, type) {
 	}
 }
 
+
 /* 修改进度后实时修改面板状态 */
 var prgList = Array.from($("#subject_prg_content").children());
-prgList.forEach(prg => {
+var handleprgList = function(prg) {
 	$(".epStatusTool a", prg).click((event) => {
 		var type = event.currentTarget.innerText;
 		if(type == '看到') {
@@ -271,4 +289,7 @@ prgList.forEach(prg => {
 		$("#cluetip-inner").append(prg_new);
 		return false;
 	})
+}
+prgList.forEach(prg => {
+	setTimeout(handleprgList, 1500, prg);
 });
