bfac86880849f50ea0d178c363a8353f19fa887d
RiverYale <1181065460@qq.com>
2023-07-09 19:09:38 +0800

    新增B站动态点赞过滤

---
+++ B站动态点赞过滤/BilibiliFilter.js
@@ -0,0 +1,93 @@
+// ==UserScript==
+// @name         Bilibili动态点赞过滤
+// @description  个人动态页面中，淡化或者隐藏点赞过的动态
+// @namespace    https://github.com/RiverYale/Userscripts/
+// @homepage     https://riveryale.github.io/Userscripts/
+// @version      0.1
+// @author       RiverYale
+// @match        *://t.bilibili.com/?*
+// @icon         https://www.bilibili.com/favicon.ico?v=1
+// @run-at       document-end
+// @compatible   chrome
+// @compatible   edge
+// @license      MIT License
+// ==/UserScript==
+
+function getAnchor() {
+	let items = document.querySelectorAll(".bili-dyn-list__item:not(:has(.like.active))");
+	let minDis = 100000;
+	let anchor = null;
+	for(let item of items) {
+		let rect = item.getBoundingClientRect();
+		let dis = Math.abs((rect.top + rect.bottom) - window.innerHeight);
+		if(dis > minDis) {
+			break;
+		} else {
+			minDis = dis;
+			anchor = item;
+		}
+	}
+	if(anchor == null || anchor.getBoundingClientRect().top > window.innerHeight || anchor.getBoundingClientRect().bottom < 0) {
+		return null;
+	}
+	return anchor;
+}
+
+setTimeout(() => {
+	var filterSidebar = document.createElement("div");
+	filterSidebar.classList.add("bili-dyn-sidebar");
+	filterSidebar.style.height = "auto";
+	filterSidebar.style.bottom = "171px";
+
+	var filterSwitch = document.createElement("div");
+	filterSwitch.classList.add("bili-dyn-sidebar__btn");
+	filterSwitch.innerHTML = "<span>过滤</span>";
+	filterSwitch.setAttribute("status", 0);
+
+	filterSidebar.appendChild(filterSwitch);
+	document.querySelector(".bili-dyn-home--member").appendChild(filterSidebar);
+
+	filterSwitch.addEventListener("click", () => {
+		let status = Number(filterSwitch.getAttribute("status"));
+		let filterCss;
+
+		let anchor = getAnchor();
+		let clientOffsetTop = anchor ? anchor.getBoundingClientRect().top : 0;
+		let headerHeight = document.querySelector(".bili-header__bar.mini-header").scrollHeight + 8;
+		let anchorY = 0;
+
+		switch(status) {
+			case 0:
+				filterCss = document.createElement('style');
+				filterCss.id = "filter-css"
+				filterCss.innerText = `.bili-dyn-list__item:has(.like.active) { filter: opacity(0.3); }`;
+				document.body.appendChild(filterCss);
+				
+				filterSwitch.style.backgroundColor = "rgb(172 223 241)";
+				filterSwitch.innerText = "淡化";
+				filterSwitch.setAttribute("status", 1);
+				break;
+			case 1:
+				filterCss = document.querySelector("#filter-css");
+				filterCss.innerText = `.bili-dyn-list__item:has(.like.active) { display: none; }`;
+
+				anchorY = anchor ? anchor.offsetTop - clientOffsetTop + headerHeight : 0;
+				document.querySelector("html").scrollTo(0, anchorY);
+
+				filterSwitch.innerText = "隐藏";
+				filterSwitch.setAttribute("status", 2);
+				break;
+			case 2:
+				filterCss = document.querySelector("#filter-css");
+				filterCss.remove();
+
+				anchorY = anchor ? anchor.offsetTop - clientOffsetTop + headerHeight : 0;
+				document.querySelector("html").scrollTo(0, anchorY);
+
+				filterSwitch.style.backgroundColor = "";
+				filterSwitch.innerText = "过滤";
+				filterSwitch.setAttribute("status", 0);
+				break;
+		}
+	})
+}, 1000);
---
+++ B站动态点赞过滤/README.md
@@ -0,0 +1,19 @@
+# 脚本说明
+
+> [在Greasy Fork上安装](https://greasyfork.org/zh-CN/scripts/470475)
+
+该脚本作用于B站个人动态页面，可以淡化或者隐藏点赞过的动态，由于B站网页不定期更新，功能可能暂时失效，请等待脚本更新。
+
+![主界面](https://riveryale.github.io/Userscripts/assets/pic/BilibiliFilter/main.png)  
+
+<br/>
+
+[GitHub Pages](https://riveryale.github.io/Userscripts/)
+
+_转载请注明出处_
+
+<br/>
+
+# 更新日志
+- v0.1
+  - 初版
